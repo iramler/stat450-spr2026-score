c3c73e74559d9287ba63a5f27542c2b4770e4b6b
elenacortinapino <elenacortinapino@gmail.com>
2026-01-21 11:29:43 -0500

    Commit

--- hotdogs/elena_scraped_table.R
+++ hotdogs/elena_scraped_table.R
@@ -4,6 +4,7 @@ install.packages("rvest")
 library(rvest)
 install.packages("dplyr")
 library(dplyr)
+library(here)
 
 url <- "https://en.wikipedia.org/wiki/Nathan%27s_Hot_Dog_Eating_Contest"
 
@@ -28,10 +29,11 @@ hotdog_table_clean <- hotdog_table %>%
     year = `Year`,
     winner = `Winner(and date, if prior to permanently moving all contests to Independence Day in 1997)`,
     dogs = `Hot dogs and buns(HDB)`,
-    time = `Contest duration`
-  ) |>
-mutate(
-    year = as.integer(year),
-    dogs = as.integer(dogs)
-  )
+    time = `Contest duration`)
+
+
+write.csv(hotdog_table_clean, "table.csv", row.names = FALSE) # this one worked
+
+clean_hotdog <- read.csv("hotdogs/hotdog_clean.csv") # relative path
+
 
