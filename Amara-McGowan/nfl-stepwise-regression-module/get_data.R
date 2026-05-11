777fe9d465fd84efb060c70e9ffd154ddd39affe
Amara-McGowan <mcgowanamara@gmail.com>
2026-05-11 10:21:46 -0400

    Updated as of 10 AM on 5/11

--- Amara-McGowan/nfl-stepwise-regression-module/get_data.R
+++ Amara-McGowan/nfl-stepwise-regression-module/get_data.R
@@ -3,49 +3,48 @@
 library(nflreadr)
 library(tidyverse)
 
-qbr <- load_espn_qbr(2025, summary_type = "week")
-schedules <- load_schedules(2025)
-stats <- load_player_stats(2025, summary_level = "week") 
-
-clean_stats <- stats |>
-  filter(position == "QB", season_type == "REG") |>
-  rename(player = player_display_name, sacks = sacks_suffered, fumbles = sack_fumbles,
-         sack_yards = sack_yards_lost) |>
-  dplyr::select(-headshot_url)
-  select(player, week, team, opponent_team, completions, attempts, passing_yards,
-         passing_tds, passing_interceptions, sacks, sack_yards, fumbles) 
-
-clean_qbr <- qbr |>
+qbr <- load_espn_qbr(2025, summary_type = "week") |>
   filter(season_type == "Regular") |>
-  rename(player = name_display, qbr = qbr_total, week = game_week) 
+  rename(player = name_display, qbr = qbr_total, week = game_week) |>
+  select(player, qbr, week)
 
-clean_qbr <- clean_qbr |>
-  dplyr::select(week, player, qbr, qb_plays)
+schedules <- load_schedules(2025) |>
+  filter(game_type == "REG")
 
-clean_schedules <- schedules |>
-  filter(game_type == "REG") |>
-  rename(team = home_team) |>
-  dplyr::select(week, team, away_team, away_score, home_score, result, total) |>
-  mutate(pct_total = (home_score / total) * 100)
+home_schedule <- schedules |>
+  rename(team = home_team, opponent_team = away_team, rest = home_rest) |>
+  mutate(pct_total = (home_score / total) * 100) |>
+  select(week, team, opponent_team, rest, roof, surface, pct_total, result)
 
-home_teams <- clean_schedules |>
-  dplyr::select(week, team, pct_total, result)
+away_schedule <- schedules |>
+  rename(team = away_team, opponent_team = home_team, rest = away_rest) |>
+  mutate(pct_total = (away_score / total) * 100) |>
+  select(week, team, opponent_team, rest, roof, surface, pct_total, result)
 
-away_teams <- clean_schedules |>
-  dplyr::select(week, away_team, home_score, away_score, total) |>
-  mutate(pct_total = (away_score / total) * 100, result = away_score - home_score) |>
-  rename(team = away_team) |>
-  dplyr::select(week, team, pct_total, result)
+stats <- load_player_stats(2025, summary_level = "week") |>
+  filter(position == "QB", season_type == "REG") |>
+  rename(player = player_display_name) |>
+  select(player, week, team, opponent_team, completions, attempts, passing_yards, 
+         passing_tds, passing_interceptions, sacks_suffered, sack_yards_lost, sack_fumbles, 
+         sack_fumbles_lost, passing_air_yards, passing_first_downs, passing_epa, passing_cpoe,
+         pacr, carries, rushing_yards, rushing_tds, rushing_fumbles, rushing_fumbles_lost, 
+         rushing_first_downs, rushing_epa)
 
-team_stats <- bind_rows(home_teams, away_teams)
+joined_schedules <- home_schedule |>
+  full_join(away_schedule)
 
-nfl_qbr <- clean_qbr |>
-  left_join(clean_stats, by = c("week", "player")) |>
-  left_join(team_stats, by = c("week", "team"))
+nfl_qbr <- qbr |>
+  left_join(stats, by = c("week", "player")) |>
+  left_join(joined_schedules, by = c("week", "team", "opponent_team")) 
+
+colSums(is.na(nfl_qbr))
+
+# To address 42 missing rushing_epa values, I put in zeros
+
+nfl_qbr <- nfl_qbr |> 
+  mutate(rushing_epa = ifelse(is.na(rushing_epa), 0, rushing_epa)) |>
+  select(-team, -opponent_team, -result, week, everything())
 
 write_csv(nfl_qbr, "nfl_qbr.csv")
 
-# Notes: 
-# - nfl_qbr has no missing values. 
-# - clean_stats has 81 unique QBs, but clean_qbr only reports QBR for 65 of them,
-#   so the final nfl_qbr dataset contains 65 players with player stats and qbr data.
+colnames(nfl_qbr)
--- Amara-McGowan/nfl-stepwise-regression-module/test.R
+++ Amara-McGowan/nfl-stepwise-regression-module/test.R
@@ -2,38 +2,27 @@ library(tidyverse)
 library(MASS)
 library(quantreg)
 
-# https://www.pro-football-reference.com/about/glossary.htm#ay/a
+nfl_qbr <- read_csv("nfl_qbr.csv") 
 
-# (Adjusted Net Yards Per Attempt (ANY/A))
+predictors <- c(
+  "completions", "attempts", "passing_yards", "passing_tds", "passing_interceptions",
+  "sacks_suffered", "sack_yards_lost", "sack_fumbles", "sack_fumbles_lost", 
+  "passing_air_yards", "passing_first_downs", "passing_epa", "passing_cpoe",
+  "pacr", "carries", "rushing_yards", "rushing_tds", 
+  "rushing_fumbles", "rushing_fumbles_lost", "rushing_first_downs", "rushing_epa",
+  "rest", "roof", "surface", "pct_total")
 
-nfl_qbr <- read_csv("nfl_qbr.csv") |>
-  mutate(comp_rate = (completions / attempts) * 100)
-
-# Testing step wise regression
-predictors <- c("comp_rate", "fumbles", "pct_total", "result",
-                "passing_yards", "passing_tds", "passing_interceptions", "sack_yards", 
-                "attempts", "sacks")
 full_mod <- lm(qbr ~ ., data = nfl_qbr[, c("qbr", predictors)])
 step_mod <- stepAIC(full_mod, direction = "both")
 summary(step_mod)
 
-
-# Testing with less things deleted
-predictors <- c("qb_plays", "comp_rate", "attempts", "passing_yards", "passing_tds", "passing_interceptions",
-"sacks", "sack_yards", "fumbles", "sack_fumbles_lost", "passing_air_yards", "passing_yards_after_catch",
-"passing_first_downs", "carries", "rushing_yards", 
-"rushing_tds", "rushing_fumbles", "rushing_fumbles_lost", "rushing_first_downs", "rushing_epa", 
-"receptions", "targets", "receiving_yards", "receiving_tds", "receiving_fumbles", "pct_total", "result")
-full_mod2 <- lm(qbr ~ ., data = nfl_qbr[, c("qbr", predictors)])
-step_mod2 <- stepAIC(full_mod, direction = "both")
-summary(step_mod) 
-
-# Testing quantile regression
 quantiles <- c(0.1, 0.25, 0.50, 0.75, 0.90)
-qr_models <- map(quantiles, ~ rq(qbr ~ comp_rate + fumbles + pct_total + passing_yards + 
-                                   passing_tds + passing_interceptions + sack_yards + attempts, tau = .x, data = nfl_qbr))
-map(qr_models, summary)
+qr_models <- map(quantiles, ~ rq(qbr ~ passing_yards + passing_tds + passing_interceptions + 
+                                   sack_yards_lost + passing_air_yards + passing_epa + passing_cpoe + 
+                                   rushing_first_downs + rushing_epa + surface + pct_total + 
+                                   sack_fumbles, tau = .x, data = nfl_qbr))
 
+map(qr_models, summary)
 
 qr_rsq <- function(model) {
   rho <- function(u, tau) tau * u * (u >= 0) + (tau - 1) * u * (u < 0)
@@ -46,6 +35,7 @@ pseudo_r2 <- map_dfr(qr_models, ~ data.frame(tau = .x$tau, r2 = qr_rsq(.x)),
 
 print(pseudo_r2) 
 
+# Plot for poster:
 ggplot(pseudo_r2, aes(x = factor(tau), y = r2)) +
   geom_col(width = 0.5, fill = "darkgreen") +
   labs(
@@ -59,6 +49,3 @@ ggplot(pseudo_r2, aes(x = factor(tau), y = r2)) +
     axis.text.y = element_text(size = 10, family = "Garamond"),
     axis.title.x = element_text(size = 12, family = "Garamond"),
     axis.title.y = element_text(size = 12, family = "Garamond"))
-
-# Most accurate for top 10% of QB ratings, okay for lower scores but tough to predict middle
-# Maybe middle is more inconsistent
